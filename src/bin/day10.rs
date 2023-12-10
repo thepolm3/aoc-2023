1b15decf31b6bb4873d410413f77c9ff31e23e20
thepolm3 <thepolm3@gmail.com>
2023-12-10 11:50:53 +0000

    day10p1

---
+++ src/bin/day10.rs
@@ -0,0 +1,181 @@
+use std::mem::size_of;
+
+use anyhow::Context;
+use itertools::{Itertools, Position};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn opposite(&self) -> Self {
+        use Direction::*;
+        match self {
+            North => South,
+            East => West,
+            South => North,
+            West => East,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Pipe {
+    NE,
+    NS,
+    NW,
+    ES,
+    EW,
+    SW,
+}
+
+impl Pipe {
+    fn from_char(x: char) -> Option<Self> {
+        use Pipe::*;
+        match x {
+            'L' => Some(NE),
+            '|' => Some(NS),
+            'J' => Some(NW),
+            'F' => Some(ES),
+            '-' => Some(EW),
+            '7' => Some(SW),
+            _ => None,
+        }
+    }
+
+    fn free_directions(&self) -> [Direction; 2] {
+        use Direction::*;
+        use Pipe::*;
+        match self {
+            NE => [North, East],
+            NS => [North, South],
+            NW => [North, West],
+            ES => [East, South],
+            EW => [East, West],
+            SW => [South, West],
+        }
+    }
+
+    fn has_access(&self, d: Direction) -> bool {
+        self.free_directions().contains(&d)
+    }
+
+    fn next_direction(&self, d: Direction) -> Direction {
+        let [a, b] = self.free_directions();
+        match d.opposite() {
+            x if x == a => b,
+            x if x == b => a,
+            _ => panic!("incorrect approach"),
+        }
+    }
+}
+struct Grid {
+    inner: Vec<Option<Pipe>>,
+    width: usize,
+    height: usize,
+    start: (usize, usize),
+}
+
+impl Grid {
+    fn from_str(input: &str) -> Option<Self> {
+        let lines: Vec<_> = input.lines().collect();
+        let width = lines.first()?.len();
+        let height = lines.len();
+        let start = if let (start_y, Some(start_x)) = lines
+            .iter()
+            .map(|line| line.chars().position(|c| c == 'S'))
+            .find_position(Option::is_some)?
+        {
+            (start_x, start_y)
+        } else {
+            unreachable!()
+        };
+
+        let inner = lines
+            .into_iter()
+            .flat_map(|line| line.chars().map(Pipe::from_char))
+            .collect();
+
+        Some(Self {
+            inner,
+            width,
+            height,
+            start,
+        })
+    }
+
+    fn get(&self, (x, y): (usize, usize)) -> Option<Pipe> {
+        if x >= self.width || y >= self.height {
+            return None;
+        }
+
+        self.inner[self.width * y + x]
+    }
+}
+
+struct Traveller<'a> {
+    position: (usize, usize),
+    heading: Direction,
+    grid: &'a Grid,
+}
+
+impl<'a> Traveller<'a> {
+    fn new(grid: &'a Grid) -> Self {
+        use Direction::*;
+        let position @ (x, y) = grid.start;
+
+        for (x, y, heading) in [
+            (x, y.saturating_sub(1), North),
+            (x, y + 1, South),
+            (x + 1, y, East),
+            (x.saturating_sub(1), y, West),
+        ] {
+            if let Some(pipe) = grid.get((x, y)) {
+                if pipe.has_access(heading.opposite()) {
+                    // println!("{:?} {:?}", position, heading);
+                    return Self {
+                        position,
+                        heading,
+                        grid,
+                    };
+                }
+            }
+        }
+
+        panic!("Invalid grid")
+    }
+}
+
+impl<'a> Iterator for Traveller<'a> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (x, y) = self.position;
+        use Direction::*;
+        self.position = match self.heading {
+            North => (x, y - 1),
+            East => (x + 1, y),
+            South => (x, y + 1),
+            West => (x - 1, y),
+        };
+
+        self.heading = self.grid.get(self.position)?.next_direction(self.heading);
+        // println!("{:?} {:?}", self.position, self.heading);
+
+        Some(self.position)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = std::fs::read_to_string("inputs/day10.txt")?;
+    let grid = Grid::from_str(&input).context("Invalid grid")?;
+
+    let traveller = Traveller::new(&grid);
+    println!("10.1: {}", (traveller.count() + 1) / 2);
+
+    Ok(())
+}
