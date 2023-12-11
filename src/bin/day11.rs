65191268926baf32b9e33e419f8d63c27ec4f130
thepolm3 <thepolm3@gmail.com>
2023-12-11 14:21:48 +0000

    small optimisations

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,33 +1,39 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 
 use itertools::Itertools;
+
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("inputs/day11.txt")?;
+
     let width = input.lines().next().unwrap().len();
     let height = input.lines().count();
-    let mut galaxies = HashSet::new();
+
+    let mut galaxies = BTreeSet::new();
+    let mut gcount = 0;
     for (y, line) in input.lines().enumerate() {
-        for (x, c) in line.chars().enumerate() {
-            if c == '#' {
+        for (x, c) in line.bytes().enumerate() {
+            if c == b'#' {
                 galaxies.insert((x, y));
+                gcount += 1;
             }
         }
     }
+
     let empty_column_contributions: usize = (0..width)
         .filter(|x| !galaxies.iter().any(|(gx, _)| gx == x))
         .map(|x| {
-            let left = galaxies.iter().filter(|(gx, _)| *gx < x).count();
-            let right = galaxies.iter().filter(|(gx, _)| *gx > x).count();
-            left * right
+            //since BTreeSet is sorted, galaxies will be in order so we look for the first galaxy
+            let left = galaxies.iter().position(|(gx, _)| *gx >= x).unwrap();
+            left * (gcount - left)
         })
         .sum();
 
     let empty_row_contributions: usize = (0..height)
         .filter(|y| !galaxies.iter().any(|(_, gy)| gy == y))
         .map(|y| {
+            //BTreeSet is not sorted for y accesses
             let above = galaxies.iter().filter(|(_, gy)| *gy < y).count();
-            let below = galaxies.iter().filter(|(_, gy)| *gy > y).count();
-            above * below
+            above * (gcount - above)
         })
         .sum();
 
@@ -36,6 +42,8 @@ fn main() -> anyhow::Result<()> {
         .combinations(2)
         .map(|v| v[1].0.abs_diff(v[0].0) + v[1].1.abs_diff(v[0].1))
         .sum();
+
+    let expansion_factor = empty_row_contributions + empty_column_contributions;
     // println!("{}", d((1, 5), (4, 9), &empty_rows, &empty_columns));
     println!(
         "11.1: {}",
@@ -43,7 +51,7 @@ fn main() -> anyhow::Result<()> {
     );
     println!(
         "11.2: {}",
-        normal_distance + (empty_column_contributions + empty_row_contributions) * (1_000_000 - 1)
+        normal_distance + expansion_factor * (1_000_000 - 1)
     );
 
     Ok(())
