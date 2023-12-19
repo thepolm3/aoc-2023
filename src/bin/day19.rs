9b6bec3fa86df2060f9db88a9c19eedc537c0322
thepolm3 <thepolm3@gmail.com>
2023-12-19 13:40:22 +0000

    day19p2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,14 +1,16 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, VecDeque},
+};
 
 use nom::{
     branch::{alt, permutation},
     bytes::complete::tag,
     character::complete::{alpha1, line_ending, one_of},
-    combinator::{map, success},
-    complete::take,
+    combinator::map,
     multi::{count, separated_list0},
     sequence::{delimited, pair, preceded, separated_pair, tuple},
-    IResult, InputTake,
+    IResult,
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -17,6 +19,100 @@ enum Property {
     M,
     A,
     S,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct RuleList<'a> {
+    list: Vec<(Constraint, &'a str)>,
+    default: &'a str,
+}
+
+impl<'a> RuleList<'a> {
+    fn map_part(&self, part: &Part) -> &str {
+        for (c, m) in &self.list {
+            if part.satisfies_constraint(c) {
+                return m;
+            }
+        }
+        self.default
+    }
+
+    fn map_range(&'a self, mut min: Part, mut max: Part) -> Vec<(&'a str, Part, Part)> {
+        let mut result: Vec<(&str, Part, Part)> = Vec::new();
+        for (Constraint(p, ordering, value), map_to) in &self.list {
+            let value = *value;
+            match ordering {
+                //keep everything less than value
+                Ordering::Less => {
+                    if min.get_property(p) >= value {
+                        //we can't satisfy the ineqality, so we continue as is
+                        continue;
+                    } else {
+                        //we can satisfy the equality on this constraint
+                        result.push((map_to, min, max.set(p, value - 1)));
+
+                        //modify for next pass
+                        min = min.set(p, value);
+                    }
+                }
+                //keep everything strictly greater than value
+                Ordering::Greater => {
+                    if max.get_property(p) <= value {
+                        //we can't satisfy the ineqality, so we continue as is
+                        continue;
+                    } else {
+                        //we can satisfy the equality on this constraint
+                        result.push((map_to, min.set(p, value + 1), max));
+
+                        //modify for next pass
+                        max = max.set(p, value);
+                    }
+                }
+                Ordering::Equal => unreachable!(),
+            }
+        }
+        result.push((self.default, min, max));
+        result
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Part {
+    x: u64,
+    m: u64,
+    a: u64,
+    s: u64,
+}
+
+impl Part {
+    fn get_property(&self, p: &Property) -> u64 {
+        use Property::*;
+        match p {
+            X => self.x,
+            M => self.m,
+            A => self.a,
+            S => self.s,
+        }
+    }
+
+    fn set(mut self, p: &Property, v: u64) -> Self {
+        use Property::*;
+        *match p {
+            X => &mut self.x,
+            M => &mut self.m,
+            A => &mut self.a,
+            S => &mut self.s,
+        } = v;
+        self
+    }
+
+    fn satisfies_constraint(&self, c: &Constraint) -> bool {
+        self.get_property(&c.0).cmp(&c.2) == c.1
+    }
+
+    fn sum(&self) -> u64 {
+        self.x + self.m + self.a + self.s
+    }
 }
 
 fn property(input: &str) -> IResult<&str, Property> {
@@ -53,23 +149,6 @@ fn rule(input: &str) -> IResult<&str, (Constraint, &str)> {
     separated_pair(constraint, tag(":"), alpha1)(input)
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct RuleList<'a> {
-    list: Vec<(Constraint, &'a str)>,
-    default: &'a str,
-}
-
-impl<'a> RuleList<'a> {
-    fn map_part(&self, part: &Part) -> &str {
-        for (c, m) in &self.list {
-            if part.satisfies_constraint(c) {
-                return m;
-            }
-        }
-        self.default
-    }
-}
-
 fn rule_list(input: &str) -> IResult<&str, RuleList> {
     map(
         delimited(
@@ -92,34 +171,6 @@ fn workflows(input: &str) -> IResult<&str, HashMap<&str, RuleList<'_>>> {
     map(separated_list0(line_ending, workflow), |rules| {
         rules.into_iter().collect()
     })(input)
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Part {
-    x: u64,
-    m: u64,
-    a: u64,
-    s: u64,
-}
-
-impl Part {
-    fn get_property(&self, p: &Property) -> u64 {
-        use Property::*;
-        match p {
-            X => self.x,
-            M => self.m,
-            A => self.a,
-            S => self.s,
-        }
-    }
-
-    fn satisfies_constraint(&self, c: &Constraint) -> bool {
-        self.get_property(&c.0).cmp(&c.2) == c.1
-    }
-
-    fn sum(&self) -> u64 {
-        self.x + self.m + self.a + self.s
-    }
 }
 
 fn part(input: &str) -> IResult<&str, Part> {
@@ -155,41 +206,56 @@ fn system(input: &str) -> IResult<&str, (HashMap<&str, RuleList<'_>>, Vec<Part>)
     separated_pair(workflows, count(line_ending, 2), parts)(input)
 }
 
-fn main() -> anyhow::Result<()> {
-    let input = "px{a<2006:qkq,m>2090:A,rfg}
-pv{a>1716:R,A}
-lnx{m>1548:A,A}
-rfg{s<537:gd,x>2440:R,A}
-qs{s>3448:A,lnx}
-qkq{x<1416:A,crn}
-crn{x>2662:A,R}
-in{s<1351:px,qqz}
-qqz{s>2770:qs,m<1801:hdj,R}
-gd{a>3333:R,R}
-hdj{m>838:A,pv}
+fn range_values(min: Part, max: Part) -> u64 {
+    (max.x - min.x + 1) * (max.m - min.m + 1) * (max.a - min.a + 1) * (max.s - min.s + 1)
+}
 
-{x=787,m=2655,a=1222,s=2876}
-{x=1679,m=44,a=2067,s=496}
-{x=2036,m=264,a=79,s=2244}
-{x=2461,m=1339,a=466,s=291}
-{x=2127,m=1623,a=2188,s=1013}";
+fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("inputs/day19.txt")?;
 
     let (_, (maps, inputs)) = system(&input).expect("invalid input");
 
-    let mut accepted = Vec::new();
+    let mut accepted = 0;
     for part in inputs {
         let mut current = "in";
         while current != "R" {
             current = maps[current].map_part(&part);
             if current == "A" {
-                accepted.push(part);
+                accepted += part.sum();
                 break;
             }
         }
     }
-    let part1 = accepted.iter().map(Part::sum).sum::<u64>();
-    println!("{:?}", part1);
+    println!("19.1: {:?}", accepted);
+
+    let mut accepted = 0;
+    let [min, max] = [
+        Part {
+            x: 1,
+            m: 1,
+            a: 1,
+            s: 1,
+        },
+        Part {
+            x: 4000,
+            m: 4000,
+            a: 4000,
+            s: 4000,
+        },
+    ];
+
+    let mut range_queue = VecDeque::from(vec![("in", min, max)]);
+    while let Some((loc, min, max)) = range_queue.pop_front() {
+        if loc == "A" {
+            accepted += range_values(min, max);
+            continue;
+        }
+        if loc == "R" {
+            continue;
+        }
+        range_queue.extend(maps[loc].map_range(min, max));
+    }
+    println!("19.2: {:?}", accepted);
 
     Ok(())
 }
